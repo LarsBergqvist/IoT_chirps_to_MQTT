6230d3a1587061fe1b324b9df7ddeafc2d13f912
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-10-26 18:48:49 +0200

    Added decoding of negative float value in the message

--- server/propagatornode/radiomessage.py
+++ server/propagatornode/radiomessage.py
@@ -22,8 +22,22 @@ class RadioMessage:
         if (self.__measurementType is None):
             return 0
         if self.__measurementType.type == "float":
-            floatVal = self.__rawdata/100.0
-            return floatVal
+            # Handle float values that can be negative from -327.67 to +327.67
+            # Bit 15 contains the sign flag,
+            # the rest of the word (max 0x7FFF) contains the float value * 100
+            data = self.__rawdata
+            floatResult = 0.0
+            if (data & 0x8000 > 0):
+                # this should be a negative value
+                data &=~(1 << 15)
+                floatResult = -data
+            else:
+                data &=~(1 << 15)
+                floatResult = data
+                
+            floatResult = floatResult/100.0            
+
+            return floatResult
         else:
             return self.__rawdata
 
