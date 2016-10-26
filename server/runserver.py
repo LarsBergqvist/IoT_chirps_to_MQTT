ba98d851218432ccd886abb77306fe00da32347e
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2016-10-26 10:15:57 +0200

    Update runserver.py

--- server/runserver.py
+++ server/runserver.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     wiringPiPinForReceiver = 2
     brokerIP = "192.168.1.16"
     brokerPort = 1883
-    node = PropagatorApplication(wiringPiPinForReceiver,brokerIP,brokerPort)
-    node.run()
+    app = PropagatorApplication(wiringPiPinForReceiver,brokerIP,brokerPort)
+    app.run()
